0fba2400fa449264376be9f1dd9eaf569f331e03
Daniel Greenfeld <pydanny@gmail.com>
2013-08-16 17:53:14 +0200

    Put in markers for configuration, plus Procfile

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/config/settings.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/config/settings.py
@@ -182,6 +182,7 @@ THIRD_PARTY_APPS = (
 # Apps specific for this project go here.
 LOCAL_APPS = (
     'users',  # custom users app
+    # Your custom apps go here
 )
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/config/urls.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/config/urls.py
@@ -21,9 +21,14 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 
+    # User management
     url(r'^users/', include("users.urls", namespace="users")),
-    url(r'^users/', include("users.urls", namespace="users")),
-    #url(r'^avatar/', include('avatar.urls')),
     url(r'^accounts/', include('allauth.urls')),
+
+    # Uncomment the next line to enable avatars
+    #url(r'^avatar/', include('avatar.urls')),
+
+    # Your custom urls go here
+
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
