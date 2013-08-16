f5e754c285bd10128abf735473b12473bc2f8325
Daniel Greenfeld <pydanny@gmail.com>
2013-08-16 16:28:23 +0200

    Getting templates and URLS to work

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/config/urls.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/config/urls.py
@@ -1,4 +1,9 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.conf import settings
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
 from django.views.generic import TemplateView
 
 # Uncomment the next two lines to enable the admin:
@@ -16,6 +21,9 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 
-    # Uncomment the next line to enable the admin:
     url(r'^users/', include("users.urls", namespace="users")),
-)
+    url(r'^users/', include("users.urls", namespace="users")),
+    #url(r'^avatar/', include('avatar.urls')),
+    url(r'^accounts/', include('allauth.urls')),
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
