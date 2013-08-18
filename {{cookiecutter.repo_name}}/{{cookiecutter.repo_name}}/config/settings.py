618cd6018a3d3deef3b512b283c166dab4b291de
Daniel Greenfeld <pydanny@gmail.com>
2013-08-18 09:44:26 +0200

    Fix authentication

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/config/settings.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/config/settings.py
@@ -177,18 +177,19 @@ WSGI_APPLICATION = 'config.wsgi.application'
 ########## End URL Configuration
 
 ########## django-secure
-INSTALLED_APPS += ("djangosecure", )
+SECURE = False
+if SECURE:
+    INSTALLED_APPS += ("djangosecure", )
 
-# set this to 60 seconds and then to 518400 when you can prove it works
-SECURE_HSTS_SECONDS = 60
-SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-SECURE_FRAME_DENY = True
-SECURE_CONTENT_TYPE_NOSNIFF = True
-SECURE_BROWSER_XSS_FILTER = True
-SESSION_COOKIE_SECURE = True
-SESSION_COOKIE_HTTPONLY = True
-SECURE_SSL_REDIRECT = True
-
+    # set this to 60 seconds and then to 518400 when you can prove it works
+    SECURE_HSTS_SECONDS = 60
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_FRAME_DENY = True
+    SECURE_CONTENT_TYPE_NOSNIFF = True
+    SECURE_BROWSER_XSS_FILTER = True
+    SESSION_COOKIE_SECURE = True
+    SESSION_COOKIE_HTTPONLY = True
+    SECURE_SSL_REDIRECT = True
 ########## end django-secure
 
 
