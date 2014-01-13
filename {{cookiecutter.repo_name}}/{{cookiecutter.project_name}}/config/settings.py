f05444762c9922e0579f259b238e9ca437827980
Fábio C. Barrionuevo da Luz <bnafta@gmail.com>
2014-01-13 15:31:33 -0200

    first load django-debug-toolbar before using it
    django-debug-toolbar is used before being loaded into INSTALLED_APPS.

--- {{cookiecutter.repo_name}}/{{cookiecutter.project_name}}/config/settings.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.project_name}}/config/settings.py
@@ -286,8 +286,8 @@ class Local(Common):
     ########## End mail settings
 
     ########## django-debug-toolbar
-    MIDDLEWARE_CLASSES = Common.MIDDLEWARE_CLASSES + ('debug_toolbar.middleware.DebugToolbarMiddleware',)
     INSTALLED_APPS += ('debug_toolbar',)
+    MIDDLEWARE_CLASSES = Common.MIDDLEWARE_CLASSES + ('debug_toolbar.middleware.DebugToolbarMiddleware',)
 
     INTERNAL_IPS = ('127.0.0.1',)
 
