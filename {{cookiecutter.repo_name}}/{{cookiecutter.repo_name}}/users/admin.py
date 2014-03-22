39d0be9b987fd36bbe5707f422c322b07570d507
Saurabh Kumar <thes.kumar@gmail.com>
2014-03-22 23:58:26 +0530

    fix(app/user): use proper UserAdmin auth form
    * use `django.contrib.auth.admin.UserAdmin`.
    * using `admin.ModelAdmin` do not provide forms to
      properly handle `passwords`
    
    [1]: https://docs.djangoproject.com/en/dev/topics/auth/customizing/#custom-users-and-the-built-in-auth-forms

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/users/admin.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/users/admin.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.admin import UserAdmin as AuthUserAdmin
 
 from .models import User
 
 
-class UserAdmin(admin.ModelAdmin):
+class UserAdmin(AuthUserAdmin):
     create_form_class = UserCreationForm
     update_form_class = UserChangeForm
 
