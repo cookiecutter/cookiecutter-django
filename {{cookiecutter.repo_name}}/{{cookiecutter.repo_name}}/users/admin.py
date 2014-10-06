5d1159174f3c0135af8a6dddda8952a1b420673c
Matt Warren <matt.warren@gmail.com>
2014-10-05 21:58:32 -0600

    addresses issue mentioned here: https://code.djangoproject.com/ticket/19353 adding users in admin resulted in reference to auth.User

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/users/admin.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/users/admin.py
@@ -6,9 +6,27 @@ from django.contrib.auth.admin import UserAdmin as AuthUserAdmin
 from .models import User
 
 
+class MyUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = User
+
+
+class MyUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = User
+
+    def clean_username(self):
+        username = self.cleaned_data["username"]
+        try:
+            User.objects.get(username=username)
+        except User.DoesNotExist:
+            return username
+        raise forms.ValidationError(self.error_messages['duplicate_username'])
+
+
 class UserAdmin(AuthUserAdmin):
-    create_form_class = UserCreationForm
-    update_form_class = UserChangeForm
+    form = MyUserChangeForm
+    add_form = MyUserCreationForm
 
 
 admin.site.register(User, UserAdmin)
