89c0fb3ff85dac4bcdee9ec2e53360869a20aa09
Saurabh Kumar <me+github@saurabh-kumar.com>
2015-04-20 02:39:00 +0530

    Replace django-configuration with django-environ
    - makes it compatible with django 1.8
    - removes magic

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/config/local.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/config/local.py
@@ -1,44 +1,54 @@
 # -*- coding: utf-8 -*-
 '''
-Local Configurations
+Local settings
 
 - Runs in Debug mode
 - Uses console backend for emails
 - Use Django Debug Toolbar
 '''
-from configurations import values
-from .common import Common
+
+from .common import *
+
+# DEBUG
+# ------------------------------------------------------------------------------
+DEBUG = env.bool('DJANGO_DEBUG', default=True)
+TEMPLATE_DEBUG = DEBUG
+
+# SECRET CONFIGURATION
+# ------------------------------------------------------------------------------
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
+# Note: This key only used for development and testing.
+SECRET_KEY = env("DJANGO_SECRET_KEY", 'CHANGEME!!!')
+
+# Mail settings
+# ------------------------------------------------------------------------------
+EMAIL_HOST = 'localhost'
+EMAIL_PORT = 1025
+EMAIL_BACKEND = env('DJANGO_EMAIL_BACKEND',
+                    default='django.core.mail.backends.console.EmailBackend')
 
 
-class Local(Common):
-
-    # DEBUG
-    DEBUG = values.BooleanValue(True)
-    TEMPLATE_DEBUG = DEBUG
-    # END DEBUG
-
-    # INSTALLED_APPS
-    INSTALLED_APPS = Common.INSTALLED_APPS
-    # END INSTALLED_APPS
-
-    # Mail settings
-    EMAIL_HOST = 'localhost'
-    EMAIL_PORT = 1025
-    EMAIL_BACKEND = values.Value('django.core.mail.backends.console.EmailBackend')
-    # End mail settings
-
-    # django-debug-toolbar
-    MIDDLEWARE_CLASSES = Common.MIDDLEWARE_CLASSES + ('debug_toolbar.middleware.DebugToolbarMiddleware',)
-    INSTALLED_APPS += ('debug_toolbar', 'django_extensions',)
-
-    INTERNAL_IPS = ('127.0.0.1', '10.0.2.2',)
-
-    DEBUG_TOOLBAR_CONFIG = {
-        'DISABLE_PANELS': [
-            'debug_toolbar.panels.redirects.RedirectsPanel',
-        ],
-        'SHOW_TEMPLATE_CONTEXT': True,
+# CACHING
+# ------------------------------------------------------------------------------
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': ''
     }
-    # end django-debug-toolbar
+}
 
-    # Your local stuff: Below this line define 3rd party library settings
+# django-debug-toolbar
+# ------------------------------------------------------------------------------
+MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
+INSTALLED_APPS += ('debug_toolbar', 'django_extensions',)
+
+INTERNAL_IPS = ('127.0.0.1', '10.0.2.2',)
+
+DEBUG_TOOLBAR_CONFIG = {
+    'DISABLE_PANELS': [
+        'debug_toolbar.panels.redirects.RedirectsPanel',
+    ],
+    'SHOW_TEMPLATE_CONTEXT': True,
+}
+
+# Your local stuff: Below this line define 3rd party library settings
--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/wsgi.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/wsgi.py
@@ -19,13 +19,12 @@ import os
 # if running multiple sites in the same mod_wsgi process. To fix this, use
 # mod_wsgi daemon mode with each site in its own daemon process, or use
 # os.environ["DJANGO_SETTINGS_MODULE"] = "{{ repo_name }}.settings"
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config")
-os.environ.setdefault("DJANGO_CONFIGURATION", "Production")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.production")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
-from configurations.wsgi import get_wsgi_application  # noqa
+from django.core.wsgi import get_wsgi_application  # noqa
 application = get_wsgi_application()
 
 # Apply WSGI middleware here.
