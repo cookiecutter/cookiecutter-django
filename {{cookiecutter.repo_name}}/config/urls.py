e07e436b461015365b2cbbdb96daa8bfc3ae31a4
Saurabh Kumar <me+github@saurabh-kumar.com>
2015-05-09 22:13:28 +0530

    Remove admin.autodiscover() call, it's called automatically in 1.7+

--- {{cookiecutter.repo_name}}/config/urls.py
+++ {{cookiecutter.repo_name}}/config/urls.py
@@ -2,19 +2,16 @@
 from __future__ import unicode_literals
 
 from django.conf import settings
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, patterns, url
 from django.conf.urls.static import static
-from django.views.generic import TemplateView
-
-# Comment the next two lines to disable the admin:
 from django.contrib import admin
-admin.autodiscover()
+from django.views.generic import TemplateView
 
 urlpatterns = patterns('',  # noqa
     url(r'^$', TemplateView.as_view(template_name='pages/home.html'), name="home"),
     url(r'^about/$', TemplateView.as_view(template_name='pages/about.html'), name="about"),
 
-    # Django Admin (Comment the next line to disable the admin)
+    # Django Admin
     url(r'^admin/', include(admin.site.urls)),
 
     # User management
