def7f475722db15deaacf2772b759a5c4ceb4f1f
Jay <ja.geb@me.com>
2015-07-16 17:43:02 +0200

    experimental docker-compose support

---
+++ {{cookiecutter.repo_name}}/compose/django/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+# setting up environment variables to work with DATABASE_URL and DJANGO_CACHE_URL
+export DJANGO_CACHE_URL=memcache://memcached:11211
+
+if [ -z "$POSTGRES_ENV_POSTGRES_USER" ]; then
+    export POSTGRES_ENV_POSTGRES_USER=postgres
+fi 
+
+export DATABASE_URL=postgres://$POSTGRES_ENV_POSTGRES_USER:$POSTGRES_ENV_POSTGRES_PASSWORD@postgres:5432/$POSTGRES_ENV_POSTGRES_USER
+{% if cookiecutter.use_celery %}
+export CELERY_BROKER_URL=amqp://guest:guest@rabbitmq:5672//
+{% endif %}
+exec "$@"
