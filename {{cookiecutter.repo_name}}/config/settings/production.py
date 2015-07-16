c9ab3b958e6d2017f6acea7970e7335670f3d8cd
jayfk <jayfk@users.noreply.github.com>
2015-07-16 11:10:36 +0200

    Fixed a bug where 'DEFAULT_FROM_EMAIL' was used to cast the value instead of providing a default

--- {{cookiecutter.repo_name}}/config/settings/production.py
+++ {{cookiecutter.repo_name}}/config/settings/production.py
@@ -106,7 +106,7 @@ EMAIL_BACKEND = 'django_mailgun.MailgunBackend'
 MAILGUN_ACCESS_KEY = env('DJANGO_MAILGUN_API_KEY')
 MAILGUN_SERVER_NAME = env('DJANGO_MAILGUN_SERVER_NAME')
 EMAIL_SUBJECT_PREFIX = env("DJANGO_EMAIL_SUBJECT_PREFIX", default='[{{cookiecutter.project_name}}] ')
-SERVER_EMAIL = env('DJANGO_SERVER_EMAIL', DEFAULT_FROM_EMAIL)
+SERVER_EMAIL = env('DJANGO_SERVER_EMAIL', default=DEFAULT_FROM_EMAIL)
 
 # TEMPLATE CONFIGURATION
 # ------------------------------------------------------------------------------
