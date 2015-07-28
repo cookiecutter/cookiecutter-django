9e3a92e3ee9f905b6decd2bdcce2561c4ec5faf8
Burhan Khalid <burhan.khalid@gmail.com>
2015-07-29 00:17:28 +0300

    adding individual test methods, fixed typo.

--- tests/test_cookiecutter_substitution.py
+++ tests/test_cookiecutter_substitution.py
@@ -1,5 +1,4 @@
-import re
-
+from __future__ import absolute_import
 import sh
 
 from .base import DjangoCookieTestCase
@@ -8,12 +7,24 @@ from .base import DjangoCookieTestCase
 class TestCookiecutterSubstitution(DjangoCookieTestCase):
     """Test that all cookiecutter instances are substituted"""
 
-    def test_all_cookiecutter_instances_are_substituted(self):
+    def test_default_configuration(self):
         # Build a list containing absolute paths to the generated files
         paths = self.generate_project()
         self.check_paths(paths)
 
-    def test_flake8_complaince(self):
+    def test_maildump_enabled(self):
+        paths = self.generate_project(extra_context={'use_maildump': 'y'})
+        self.check_paths(paths)
+
+    def test_celery_enabled(self):
+        paths = self.generate_project(extra_context={'use_celery': 'y'})
+        self.check_paths(paths)
+
+    def test_windows_enabled(self):
+        paths = self.generate_project(extra_context={'windows': 'y'})
+        self.check_paths(paths)
+
+    def test_flake8_compliance(self):
         """generated project should pass flake8"""
         self.generate_project()
         try:
