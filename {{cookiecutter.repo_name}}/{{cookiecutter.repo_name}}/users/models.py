518c5d561a012119bbd048b947065222c835cf34
Daniel Greenfeld <danny@eventbrite.com>
2015-08-05 18:32:12 -0700

    making the user object python 2 and 3 friendly

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/users/models.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/users/models.py
@@ -4,16 +4,18 @@ from __future__ import unicode_literals, absolute_import
 from django.contrib.auth.models import AbstractUser
 from django.core.urlresolvers import reverse
 from django.db import models
-# from django.utils.translation import ugettext_lazy as _
+from django.utils.encoding import python_2_unicode_compatible
+from django.utils.translation import ugettext_lazy as _
 
 
+@python_2_unicode_compatible
 class User(AbstractUser):
 
     # First Name and Last Name do not cover name patterns
     # around the globe.
-    name = models.CharField("Name of User", blank=True, max_length=255)
+    name = models.CharField(_("Name of User)", blank=True, max_length=255)
 
-    def __unicode__(self):
+    def __str__(self):
         return self.username
 
     def get_absolute_url(self):
