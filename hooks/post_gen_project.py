ddaf523cd53df8105a4af7d26d699923aceb68d5
Krzysztof Szumny <krzysztof.szumny@spistresci.pl>
2015-08-18 17:47:44 +0200

    PyCharm - WIP

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -4,6 +4,7 @@ import shutil
 
 project_directory = os.path.realpath(os.path.curdir)
 
+# ------------------------------------------------------------------------------
 docker = '{{cookiecutter.use_docker}}' == 'y'
 
 docker_files = [
@@ -23,3 +24,23 @@ if not docker:
 
     for path in docker_dirs:
         shutil.rmtree(os.path.join(project_directory, path))
+
+# ------------------------------------------------------------------------------
+pycharm = '{{cookiecutter.use_pycharm}}' == 'y'
+
+pycharm_files = [
+
+]
+
+pycharm_dirs = [
+    '.idea/',
+]
+
+if not pycharm:
+    for path in pycharm_files:
+        os.remove(os.path.join(project_directory, path))
+
+    for path in pycharm_dirs:
+        shutil.rmtree(os.path.join(project_directory, path))
+
+# ------------------------------------------------------------------------------
