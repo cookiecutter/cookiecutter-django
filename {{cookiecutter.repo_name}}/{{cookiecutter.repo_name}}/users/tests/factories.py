e1d76ea25ee164a1241cb66ee6d862b7eb6f8ef1
Adam Dobrawy <ad-m@users.noreply.github.com>
2015-10-18 13:58:41 +0200

    Add factories for users

---
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/users/tests/factories.py
@@ -0,0 +1,10 @@
+from feder.users import models
+import factory
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    username = factory.Sequence(lambda n: 'user-{0}'.format(n))
+    email = factory.Sequence(lambda n: 'user-{0}@example.com'.format(n))
+
+    class Meta:
+        model = models.User
