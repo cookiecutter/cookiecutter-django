88c6a1775b04bee1d2a95aa09d2411a4211169a5
Adam Dobrawy <ad-m@users.noreply.github.com>
2015-11-12 05:51:15 +0100

    Added default password to UserFactory

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/users/tests/factories.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/users/tests/factories.py
@@ -5,6 +5,8 @@ import factory
 class UserFactory(factory.django.DjangoModelFactory):
     username = factory.Sequence(lambda n: 'user-{0}'.format(n))
     email = factory.Sequence(lambda n: 'user-{0}@example.com'.format(n))
+    password = factory.PosteGnerationMethodCall('set_password', 'password')
 
     class Meta:
-        model = models.User
+        model = 'users.User'
+        django_get_or_create = ('username', )
