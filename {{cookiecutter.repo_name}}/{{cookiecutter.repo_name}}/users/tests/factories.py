c86d5c928cdefd09aca102b2a5c37f662e1426a6
Adam Dobrawy <ad-m@users.noreply.github.com>
2015-11-14 14:15:00 +0100

    Fix typo & import in UserFactory

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/users/tests/factories.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/users/tests/factories.py
@@ -1,12 +1,11 @@
-from feder.users import models
 import factory
 
 
 class UserFactory(factory.django.DjangoModelFactory):
     username = factory.Sequence(lambda n: 'user-{0}'.format(n))
     email = factory.Sequence(lambda n: 'user-{0}@example.com'.format(n))
-    password = factory.PosteGnerationMethodCall('set_password', 'password')
+    password = factory.PostGenerationMethodCall('set_password', 'password')
 
     class Meta:
-        model = models.User
+        model = 'users.User'
         django_get_or_create = ('username', )
