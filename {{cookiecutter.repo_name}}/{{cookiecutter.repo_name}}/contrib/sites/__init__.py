f78069f523153f65b3b5398b683f0bc837b6dad4
Daniel Roy Greenfeld <pydanny@gmail.com>
2016-01-16 10:43:01 -0800

    Fixes #345 explain need for django.contrib.sites

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/contrib/sites/__init__.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/contrib/sites/__init__.py
@@ -1,1 +1,3 @@
 # -*- coding: utf-8 -*-
+
+# To understand why this file is here, please read http://cookiecutter-django.readthedocs.org/en/latest/faq.html#why-is-there-a-django-contrib-sites-directory-in-cookiecutter-django
--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/contrib/sites/migrations/0002_set_site_domain_and_name.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/contrib/sites/migrations/0002_set_site_domain_and_name.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+
+# To understand why this file is here, please read http://cookiecutter-django.readthedocs.org/en/latest/faq.html#why-is-there-a-django-contrib-sites-directory-in-cookiecutter-django
+
 from __future__ import unicode_literals
 
 from django.conf import settings
