b66ec6594ee0cdac4a0909ef614dc64d0829fd33
Daniel Roy Greenfeld <pydanny@gmail.com>
2016-01-16 11:00:56 -0800

    Fix test breakers

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/contrib/sites/migrations/0002_set_site_domain_and_name.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/contrib/sites/migrations/0002_set_site_domain_and_name.py
@@ -1,6 +1,9 @@
-# -*- coding: utf-8 -*-
+"""
+To understand why this file is here, please read:
 
-# To understand why this file is here, please read http://cookiecutter-django.readthedocs.org/en/latest/faq.html#why-is-there-a-django-contrib-sites-directory-in-cookiecutter-django
+http://cookiecutter-django.readthedocs.org/en/latest/faq.html#why-is-there-a-django-contrib-sites-directory-in-cookiecutter-django
+"""
+# -*- coding: utf-8 -*-
 
 from __future__ import unicode_literals
 
