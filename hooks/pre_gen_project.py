fa89445f6668c7668937c3f6b1d93f10ef557206
Sulé <sule.cm@gmail.com>
2016-06-03 16:35:10 -0700

    Removing unnecessary version check #534 (#584)
    * Address issue #534
    
    * missing comma added
    
    * remove unecesssary version check #534

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,8 +1,6 @@
-import cookiecutter
-
 project_slug = '{{ cookiecutter.project_slug }}'
 
 if hasattr(project_slug, 'isidentifier'):
     assert project_slug.isidentifier(), 'Project slug should be valid Python identifier!'
 
-assert cookiecutter.__version__ > '1.3.0', 'Please upgrade your Cookiecutter installation'
+
