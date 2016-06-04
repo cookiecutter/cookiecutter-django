8a925db024b114930eacd3caa1509496516183a3
Vivian Guillen <vivian@viviangb.com>
2016-06-03 17:05:57 -0700

    Add gulp alternative as a js task runner

--- {{cookiecutter.project_slug}}/gulpfile.js
+++ {{cookiecutter.project_slug}}/gulpfile.js
@@ -14,6 +14,10 @@ var gulp = require('gulp'),
       del = require('del'),
       plumber = require('gulp-plumber'),
       pixrem = require('gulp-pixrem'),
+      uglify = require('gulp-uglify'),
+      imagemin = require('gulp-imagemin'),
+      exec = require('gulp-exec'),
+      runSequence = require('run-sequence'),
       browserSync = require('browser-sync');
 
 
@@ -38,15 +42,66 @@ var paths = pathsConfig();
 		//Tasks//
 ////////////////////////////////
 
-// Styles
+// Styles autoprefixing and minification
 gulp.task('styles', function() {
-  return gulp.src(paths.sass + '/project.scss', { style: 'expanded' })
+  return gulp.src(paths.sass + '/project.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(plumber()) // It checks for errors
+    .pipe(plumber()) // Checks for errors
     .pipe(autoprefixer({browsers: ['last 2 version']})) // Adds vendor prefixes
     .pipe(pixrem())  // add fallbacks for rem units
-    .pipe(gulp.dest('./static/css/'))
+    .pipe(gulp.dest(paths.css))
     .pipe(rename({ suffix: '.min' }))
     .pipe(cssnano()) // Minifies the result
-    .pipe(gulp.dest('./static/css/'));
+    .pipe(gulp.dest(paths.css));
+});
+
+// Javascript minification
+gulp.task('scripts', function() {
+  return gulp.src(paths.js + '/project.js')
+    .pipe(plumber()) // Checks for errors
+    .pipe(uglify()) // Minifies the js
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest(paths.js));
+});
+
+// Image compression
+gulp.task('imgCompression', function(){
+  return gulp.src(paths.images + '/*')
+    .pipe(imagemin()) // Compresses PNG, JPEG, GIF and SVG images
+    .pipe(gulp.dest(paths.images))
+});
+
+// Run django server
+gulp.task('runServer', function() {
+  exec('python manage.py runserver', function (err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+  });
+});
+
+// Browser sync server for live reload
+gulp.task('browserSync', function() {
+    browserSync.init(
+      [paths.css + "/*.css", paths.js + "*.js", paths.templates + '*.html'], {
+        proxy:  "localhost:8000"
+    });
+});
+
+// Default task
+gulp.task('default', function() {
+    runSequence(['styles', 'scripts', 'imgCompression'], 'runServer', 'browserSync');
+});
+
+////////////////////////////////
+		//Watch//
+////////////////////////////////
+
+// Watch
+gulp.task('watch', ['default'], function() {
+
+  gulp.watch(paths.sass + '/*.scss', ['styles']);
+  gulp.watch(paths.js + '/*.js', ['scripts']);
+  gulp.watch(paths.images + '/*', ['imgCompression']);
+  gulp.watch('templates/*.html');
+
 });
