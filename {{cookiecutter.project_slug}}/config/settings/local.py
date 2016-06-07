0a55b3d7c0e815a4154fa885bb4b6ca85630ea00
Julien Almarcha <julien.almarcha@gmail.com>
2016-06-07 18:04:30 +0200

    Update local.py

--- {{cookiecutter.project_slug}}/config/settings/local.py
+++ {{cookiecutter.project_slug}}/config/settings/local.py
@@ -51,7 +51,7 @@ INSTALLED_APPS += ('debug_toolbar', )
 INTERNAL_IPS = ['127.0.0.1', '10.0.2.2',]
 # tricks to have debug toolbar when developing with docker
 ip = socket.gethostbyname(socket.gethostname())
-INTERNAL_IPS += [ip, ip[:-1]+"1"]
+INTERNAL_IPS += [ip[:-1]+"1"]
 
 DEBUG_TOOLBAR_CONFIG = {
     'DISABLE_PANELS': [
