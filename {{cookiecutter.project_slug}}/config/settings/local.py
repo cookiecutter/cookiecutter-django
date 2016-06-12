bd3a61c0cebfef768cffaa721c9eee8f33e32109
Julien Almarcha <julien.almarcha@gmail.com>
2016-06-12 20:45:12 +0200

    flake8 compliance

--- {{cookiecutter.project_slug}}/config/settings/local.py
+++ {{cookiecutter.project_slug}}/config/settings/local.py
@@ -51,7 +51,7 @@ INSTALLED_APPS += ('debug_toolbar', )
 
 INTERNAL_IPS = ['127.0.0.1', '10.0.2.2', ]
 # tricks to have debug toolbar when developing with docker
-if  os.environ.get('USE_DOCKER') == 'yes' :
+if  os.environ.get('USE_DOCKER') == 'yes':
     ip = socket.gethostbyname(socket.gethostname())
     INTERNAL_IPS += [ip[:-1]+"1"]
 
