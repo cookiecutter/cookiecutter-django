13faa217999bcfef511602685b3d4bf938cfb73d
Julien Almarcha <julien.almarcha@gmail.com>
2016-06-18 04:09:08 +0200

    automatically update INTERNAL_IPS (#600)
    * change local config to automatically update INTERNAL_IPS when developping with docker
    
    * Update local.py
    
    Move import to top
    
    * Update local.py
    
    Bug fix and remove blank line
    
    * Update local.py
    
    * flake8 compliance
    
    * add env var check before updating INTERNAL_IPS
    
    * flake8 compliance
    
    * flake8 compliance

--- {{cookiecutter.project_slug}}/config/settings/local.py
+++ {{cookiecutter.project_slug}}/config/settings/local.py
@@ -9,6 +9,8 @@ Local settings
 """
 
 from .common import *  # noqa
+import socket
+import os
 
 # DEBUG
 # ------------------------------------------------------------------------------
@@ -47,7 +49,11 @@ CACHES = {
 MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
 INSTALLED_APPS += ('debug_toolbar', )
 
-INTERNAL_IPS = ('127.0.0.1', '10.0.2.2',)
+INTERNAL_IPS = ['127.0.0.1', '10.0.2.2', ]
+# tricks to have debug toolbar when developing with docker
+if os.environ.get('USE_DOCKER') == 'yes':
+    ip = socket.gethostbyname(socket.gethostname())
+    INTERNAL_IPS += [ip[:-1]+"1"]
 
 DEBUG_TOOLBAR_CONFIG = {
     'DISABLE_PANELS': [
