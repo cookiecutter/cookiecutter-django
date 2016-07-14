b1e7f55b6bb7b18cfdcb4e7abcec5489fe5b4d1a
Daniel Roy Greenfeld <pydanny@users.noreply.github.com>
2016-07-14 12:06:29 -0700

    Update common.py

--- {{cookiecutter.project_slug}}/config/settings/common.py
+++ {{cookiecutter.project_slug}}/config/settings/common.py
@@ -231,6 +231,10 @@ INSTALLED_APPS += ('{{cookiecutter.project_slug}}.taskapp.celery.CeleryConfig',)
 # if you are not using the django database broker (e.g. rabbitmq, redis, memcached), you can remove the next line.
 INSTALLED_APPS += ('kombu.transport.django',)
 BROKER_URL = env('CELERY_BROKER_URL', default='django://')
+if BROKER_URL == 'django://':
+    CELERY_RESULT_BACKEND = 'redis://'
+else:
+    CELERY_RESULT_BACKEND = BROKER_URL
 ########## END CELERY
 {% endif %}
 
