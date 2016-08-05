68ebee951d55bfdef5af1c0c17d6dc71092890e8
yunti <yunti9-mail@yahoo.com>
2016-08-05 22:03:06 +0100

    Update crispy forms template pack
    Updated to be inline with bootstrap 4 currently used in the project

--- {{cookiecutter.project_slug}}/config/settings/common.py
+++ {{cookiecutter.project_slug}}/config/settings/common.py
@@ -165,7 +165,7 @@ TEMPLATES = [
 ]
 
 # See: http://django-crispy-forms.readthedocs.io/en/latest/install.html#template-packs
-CRISPY_TEMPLATE_PACK = 'bootstrap3'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # STATIC FILE CONFIGURATION
 # ------------------------------------------------------------------------------
