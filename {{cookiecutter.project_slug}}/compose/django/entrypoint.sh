34631ad819ba63ba2dccebb4c867838371731c7a
Jannis Gebauer <jayfk@users.noreply.github.com>
2016-08-16 21:20:41 +0200

    adds the ability to wait for the postgres container (#736)

--- {{cookiecutter.project_slug}}/compose/django/entrypoint.sh
+++ {{cookiecutter.project_slug}}/compose/django/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -e
+cmd="$@"
+
 # This entrypoint is used to play nicely with the current cookiecutter configuration.
 # Since docker-compose relies heavily on environment variables itself for configuration, we'd have to define multiple
 # environment variables just to support cookiecutter out of the box. That makes no sense, so this little entrypoint
@@ -15,4 +17,23 @@ export DATABASE_URL=postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@postgres:5432/$
 {% if cookiecutter.use_celery == 'y' %}
 export CELERY_BROKER_URL=$REDIS_URL/0
 {% endif %}
-exec "$@"
+
+function postgres_ready(){
+python << END
+import sys
+import psycopg2
+try:
+    conn = psycopg2.connect(dbname="$POSTGRES_USER", user="$POSTGRES_USER", password="$POSTGRES_PASSWORD", host="postgres")
+except psycopg2.OperationalError:
+    sys.exit(-1)
+sys.exit(0)
+END
+}
+
+until postgres_ready; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Postgres is up - continuing..."
+exec $cmd
