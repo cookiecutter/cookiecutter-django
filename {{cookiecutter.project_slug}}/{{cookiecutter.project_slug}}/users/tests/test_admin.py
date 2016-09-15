62dcd62be89a1a6e585715446e008cc7d1cdfc40
Steve Steiner <ssteinerX@gmail.com>
2016-09-15 12:40:15 -0400

    Make test_docker.sh tests pass by passing new password auth rules

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/tests/test_admin.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/tests/test_admin.py
@@ -6,14 +6,14 @@ from ..admin import MyUserCreationForm
 class TestMyUserCreationForm(TestCase):
 
     def setUp(self):
-        self.user = self.make_user()
+        self.user = self.make_user('notalamode', 'notalamodespassword')
 
     def test_clean_username_success(self):
         # Instantiate the form with a new username
         form = MyUserCreationForm({
             'username': 'alamode',
-            'password1': '123456',
-            'password2': '123456',
+            'password1': '7jefB#f@Cc7YJB]2v',
+            'password2': '7jefB#f@Cc7YJB]2v',
         })
         # Run is_valid() to trigger the validation
         valid = form.is_valid()
@@ -27,8 +27,8 @@ class TestMyUserCreationForm(TestCase):
         # Instantiate the form with the same username as self.user
         form = MyUserCreationForm({
             'username': self.user.username,
-            'password1': '123456',
-            'password2': '123456',
+            'password1': 'notalamodespassword',
+            'password2': 'notalamodespassword',
         })
         # Run is_valid() to trigger the validation, which is going to fail
         # because the username is already taken
