a36dc8c0e23329caea8ac15fb53d65a4b7fe529b
Fabio C. Barrioneuvo da Luz <bnafta@gmail.com>
2016-09-15 00:30:23 -0300

    add LocaleMiddleware to fix issue #792

--- {{cookiecutter.project_slug}}/config/settings/common.py
+++ {{cookiecutter.project_slug}}/config/settings/common.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 MIDDLEWARE = (
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
