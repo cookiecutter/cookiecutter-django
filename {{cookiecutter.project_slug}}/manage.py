32169f0cbf90b4b40f84faa89240ec1705f69ae8
Fabio C. Barrioneuvo da Luz <bnafta@gmail.com>
2016-09-15 00:56:48 -0300

    ignore F401 error on manage.py

--- {{cookiecutter.project_slug}}/manage.py
+++ {{cookiecutter.project_slug}}/manage.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
         # issue is really that Django is missing to avoid masking other
         # exceptions on Python 2.
         try:
-            import django
+            import django  # noqa
         except ImportError:
             raise ImportError(
                 "Couldn't import Django. Are you sure it's installed and "
