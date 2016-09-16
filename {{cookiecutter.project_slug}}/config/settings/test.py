da42b3854d85b1df42c67e4e5f3d9131aacecd2c
Audrey Roy Greenfeld <aroy@alum.mit.edu>
2016-09-16 11:05:57 -0700

    Turn on template debugging in test settings
    This resolves the following error:
    
    ``` bash
    coverage run ./manage.py test --settings=config.settings.test
    Coverage.py warning: Disabling plugin 'django_coverage_plugin.DjangoTemplatePlugin' due to an exception:
    Traceback (most recent call last):
      File "/Users/audreyr/.virtualenvs/everycheese/lib/python3.5/site-packages/coverage/control.py", line 517, in _should_trace_internal
        file_tracer = plugin.file_tracer(canonical)
      File "/Users/audreyr/.virtualenvs/everycheese/lib/python3.5/site-packages/django_coverage_plugin/plugin.py", line 154, in file_tracer
        check_debug()
      File "/Users/audreyr/.virtualenvs/everycheese/lib/python3.5/site-packages/django_coverage_plugin/plugin.py", line 69, in check_debug
        "Template debugging must be enabled in settings."
    django_coverage_plugin.plugin.DjangoTemplatePluginException: Template debugging must be enabled in settings.
    ```

--- {{cookiecutter.project_slug}}/config/settings/test.py
+++ {{cookiecutter.project_slug}}/config/settings/test.py
@@ -12,7 +12,8 @@ from .common import *  # noqa
 # ------------------------------------------------------------------------------
 # Turn debug off so tests run faster
 DEBUG = False
-TEMPLATES[0]['OPTIONS']['debug'] = False
+# But template debugging must be enabled for django_coverage_plugin
+TEMPLATES[0]['OPTIONS']['debug'] = True
 
 # SECRET CONFIGURATION
 # ------------------------------------------------------------------------------
