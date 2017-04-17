8b82dfae8a2bcb61a3e4bda28e650f810e97dca2
Daniel Roy Greenfeld <pydanny@users.noreply.github.com>
2017-04-17 10:51:58 -0700

    Change order of modern django debug toolbar.

--- {{cookiecutter.project_slug}}/config/urls.py
+++ {{cookiecutter.project_slug}}/config/urls.py
@@ -36,6 +36,7 @@ if settings.DEBUG:
     if 'debug_toolbar' in settings.INSTALLED_APPS:
         import debug_toolbar
 
-        urlpatterns += [
+        urlpatterns = [
             url(r'^__debug__/', include(debug_toolbar.urls)),
-        ]
+        ] + urlpatterns
+
