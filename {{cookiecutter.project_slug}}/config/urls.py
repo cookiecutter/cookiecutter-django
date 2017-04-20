b0fe04fc52df6bf0ab842839689a517d4d2ad3b1
Daniel Roy Greenfeld <pydanny@gmail.com>
2017-04-20 15:18:30 -0700

    First pass at Bootstrap 4 Alpha 6, just the basic layout

--- {{cookiecutter.project_slug}}/config/urls.py
+++ {{cookiecutter.project_slug}}/config/urls.py
@@ -35,8 +35,6 @@ if settings.DEBUG:
     ]
     if 'debug_toolbar' in settings.INSTALLED_APPS:
         import debug_toolbar
-
         urlpatterns = [
             url(r'^__debug__/', include(debug_toolbar.urls)),
         ] + urlpatterns
-
