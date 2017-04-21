5944ced7a425f9c895adcf50e4f39e7db9d9698b
Daniel Roy Greenfeld <pydanny@gmail.com>
2017-04-21 08:46:09 -0700

    Remove Python 2.7 support per

--- README.rst
+++ README.rst
@@ -128,7 +128,6 @@ Answer the prompts with your own desired options_. For example::
     use_opbeat [n]: y
     use_pycharm [n]: y
     windows [n]: n
-    use_python3 [y]: y
     use_docker [y]: n
     use_heroku [n]: y
     use_compressor [n]: y
--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/contrib/sites/migrations/0003_set_site_domain_and_name.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/contrib/sites/migrations/0003_set_site_domain_and_name.py
@@ -3,10 +3,6 @@ To understand why this file is here, please read:
 
 http://cookiecutter-django.readthedocs.io/en/latest/faq.html#why-is-there-a-django-contrib-sites-directory-in-cookiecutter-django
 """
-# -*- coding: utf-8 -*-
-
-from __future__ import unicode_literals
-
 from django.conf import settings
 from django.db import migrations
 
