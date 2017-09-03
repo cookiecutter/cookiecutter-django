4e5a8c8119cf94ecc12104fb336ba7f062d18e0a
Nikita P. Shupeyko <webyneter@gmail.com>
2017-09-03 12:00:51 +0300

    Use Gulp in a test-generated project

--- tests/test_docker.sh
+++ tests/test_docker.sh
@@ -1,21 +1,16 @@
 #!/bin/sh
-# this is a very simple script that tests the docker configuration for cookiecutter-django
-# it is meant to be run from the root directory of the repository, eg:
-# sh tests/test_docker.sh
 
-# install test requirements
-pip install -r requirements.txt
+pip install -r requirements/production.txt
 
-# create a cache directory
 mkdir -p .cache/docker
 cd .cache/docker
 
 # create the project using the default settings in cookiecutter.json
-cookiecutter ../../ --no-input --overwrite-if-exists use_docker=y js_task_runner=None
+cookiecutter ../../ \
+    --no-input \
+    --overwrite-if-exists use_docker=y
 cd project_name
 
-# run the project's tests
-docker-compose -f local.yml run django python manage.py test
+docker-compose -f local.yml run --rm django pytest
 
-# return non-zero status code if there are migrations that have not been created
-docker-compose -f local.yml run django python manage.py makemigrations --dry-run --check || { echo "ERROR: there were changes in the models, but migration listed above have not been created and are not saved in version control"; exit 1; }
+docker-compose -f local.yml run --rm django python manage.py makemigrations --dry-run --check || { echo "ERROR: there were changes in the models, but migration listed above have not been created and are not saved in version control"; exit 1; }
