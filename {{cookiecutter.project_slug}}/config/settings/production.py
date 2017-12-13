d2dd265a9968740ddf6e59686c4ec520e4d52ff2
Pierre Chiquet <pchiquet@gmail.com>
2017-12-13 14:06:48 +0100

    Fix MediaRootS3BotoStorage: do not override existing files (#1367)
    * Fix MediaRootS3BotoStorage: do not override existing files
    
    see https://github.com/jschneier/django-storages/issues/50
    
    * Update "other contributors"
    
    * alphabetical order

--- {{cookiecutter.project_slug}}/config/settings/production.py
+++ {{cookiecutter.project_slug}}/config/settings/production.py
@@ -120,7 +120,7 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 #  See:http://stackoverflow.com/questions/10390244/
 from storages.backends.s3boto3 import S3Boto3Storage
 StaticRootS3BotoStorage = lambda: S3Boto3Storage(location='static')  # noqa
-MediaRootS3BotoStorage = lambda: S3Boto3Storage(location='media')  # noqa
+MediaRootS3BotoStorage = lambda: S3Boto3Storage(location='media', file_overwrite=False)  # noqa
 DEFAULT_FILE_STORAGE = 'config.settings.production.MediaRootS3BotoStorage'
 
 MEDIA_URL = 'https://s3.amazonaws.com/%s/media/' % AWS_STORAGE_BUCKET_NAME
