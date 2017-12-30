8bfe18b52ad1a48abe5ae0346b19a96a596aec64
Dayson Pais <dayson@crowdfireapp.com>
2017-12-30 12:16:19 +0530

    Update django from 1.11.8 to 2.0
    - Refactor django.core.urlresolvers module import to django.urls. Resolves: The django.core.urlresolvers module is removed in favor of its new location, django.urls.
    
    - Add an app namespace to urls in user app. Resolves: Support for setting a URL instance namespace without an application namespace is removed.

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/models.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/models.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import AbstractUser
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
