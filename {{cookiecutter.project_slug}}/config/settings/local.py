36de85375f7a3b7a9854f30c07dd1db446968fd3
Wan Liuyang <tsfdye@gmail.com>
2018-01-12 17:54:49 +0800

    Update celery to 4.1.0

--- {{cookiecutter.project_slug}}/config/settings/local.py
+++ {{cookiecutter.project_slug}}/config/settings/local.py
@@ -81,7 +81,7 @@ TEST_RUNNER = 'django.test.runner.DiscoverRunner'
 {% if cookiecutter.use_celery == 'y' %}
 ########## CELERY
 # In development, all tasks will be executed locally by blocking until the task returns
-CELERY_ALWAYS_EAGER = True
+CELERY_TASK_ALWAYS_EAGER = True
 ########## END CELERY
 {% endif %}
 # Your local stuff: Below this line define 3rd party library settings
