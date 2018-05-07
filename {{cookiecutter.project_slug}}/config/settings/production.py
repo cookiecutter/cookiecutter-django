00ff5c8506f3a100addd84eb25bf340c8a01ea18
Nikita P. Shupeyko <webyneter@gmail.com>
2018-05-07 20:33:34 +0300

    Fix a typo in production.py

--- {{cookiecutter.project_slug}}/config/settings/production.py
+++ {{cookiecutter.project_slug}}/config/settings/production.py
@@ -245,7 +245,7 @@ RAVEN_CONFIG = {
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#logging
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
-# the site admins bon every HTTP 500 error when DEBUG=False.
+# the site admins on every HTTP 500 error when DEBUG=False.
 # See https://docs.djangoproject.com/en/dev/topics/logging for
 # more details on how to customize your logging configuration.
 LOGGING = {
