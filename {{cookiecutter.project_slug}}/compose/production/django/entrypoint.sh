17739ff2630f0ead43065065f6b850d9ef2d5a9b
Nikita P. Shupeyko <webyneter@gmail.com>
2018-05-09 12:58:37 +0300

    Refactor POSTGRES_PORT env

--- {{cookiecutter.project_slug}}/compose/production/django/entrypoint.sh
+++ {{cookiecutter.project_slug}}/compose/production/django/entrypoint.sh
@@ -14,7 +14,7 @@ if [ -z "${POSTGRES_USER}" ]; then
     base_postgres_image_default_user='postgres'
     export POSTGRES_USER="${base_postgres_image_default_user}"
 fi
-export DATABASE_URL="postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:5432/${POSTGRES_DB}"
+export DATABASE_URL="postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}"
 
 postgres_ready() {
 python << END
@@ -27,7 +27,8 @@ try:
         dbname="${POSTGRES_DB}",
         user="${POSTGRES_USER}",
         password="${POSTGRES_PASSWORD}",
-        host="${POSTGRES_HOST}"
+        host="${POSTGRES_HOST}",
+        port="${POSTGRES_PORT}",
     )
 except psycopg2.OperationalError:
     sys.exit(-1)
