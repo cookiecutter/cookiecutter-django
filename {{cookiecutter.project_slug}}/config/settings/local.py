df1ce737d5d8744241e6d1d804a3e3b7b9379d76
Nikita P. Shupeyko <webyneter@gmail.com>
2018-05-09 13:15:35 +0300

    Replace os.environ.get with env call in local.py

--- {{cookiecutter.project_slug}}/config/settings/local.py
+++ {{cookiecutter.project_slug}}/config/settings/local.py
@@ -62,9 +62,8 @@ DEBUG_TOOLBAR_CONFIG = {
 # https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#internal-ips
 INTERNAL_IPS = ['127.0.0.1', '10.0.2.2']
 {% if cookiecutter.use_docker == 'y' -%}
-import socket
-import os
-if os.environ.get('USE_DOCKER') == 'yes':
+if env('USE_DOCKER') == 'yes':
+    import socket
     hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
     INTERNAL_IPS += [ip[:-1] + '1' for ip in ips]
 {%- endif %}
