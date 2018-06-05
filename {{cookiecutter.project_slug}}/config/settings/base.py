ca6b073fd8c8c03c487b97b61206c659b71c5047
Nikita P. Shupeyko <webyneter@gmail.com>
2018-06-05 12:54:41 +0300

    CELERY_TASK_SOFT_TIME_LIMIT

--- {{cookiecutter.project_slug}}/config/settings/base.py
+++ {{cookiecutter.project_slug}}/config/settings/base.py
@@ -245,6 +245,9 @@ CELERY_RESULT_SERIALIZER = 'json'
 # http://docs.celeryproject.org/en/latest/userguide/configuration.html#task-time-limit
 # TODO: set to whatever value is adequate in your circumstances
 CELERY_TASK_TIME_LIMIT = 5 * 60
+# http://docs.celeryproject.org/en/latest/userguide/configuration.html#task-soft-time-limit
+# TODO: set to whatever value is adequate in your circumstances
+CELERY_TASK_SOFT_TIME_LIMIT = 60
 
 {%- endif %}
 # django-allauth
