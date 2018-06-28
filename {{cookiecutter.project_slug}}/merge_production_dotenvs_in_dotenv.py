9ae0f2f4b370cf95edbc87cc96c1d57bcc93cfeb
Nikita P. Shupeyko <webyneter@gmail.com>
2018-06-28 11:09:05 +0300

    Fix envs path in merge_production_dotenvs_in_dotenv.py

--- {{cookiecutter.project_slug}}/merge_production_dotenvs_in_dotenv.py
+++ {{cookiecutter.project_slug}}/merge_production_dotenvs_in_dotenv.py
@@ -4,7 +4,7 @@ from typing import Sequence
 import pytest
 
 ROOT_DIR_PATH = os.path.dirname(os.path.realpath(__file__))
-PRODUCTION_DOTENVS_DIR_PATH = os.path.join(ROOT_DIR_PATH, ".envs", ".production")
+PRODUCTION_DOTENVS_DIR_PATH = os.path.join(ROOT_DIR_PATH, ".envs")
 PRODUCTION_DOTENV_FILE_PATHS = [
     os.path.join(PRODUCTION_DOTENVS_DIR_PATH, ".django"),
     os.path.join(PRODUCTION_DOTENVS_DIR_PATH, ".postgres"),
