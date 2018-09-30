9cf2218af9bf2abe0344f0736991735f395d17bb
myvault <vaultofpython@outlook.com>
2018-09-30 11:14:41 +0300

    Removing ignored files

--- {{cookiecutter.project_slug}}/sc/create_db.sh
+++ {{cookiecutter.project_slug}}/sc/create_db.sh
@@ -3,25 +3,25 @@ sudo -u postgres psql --command 'CREATE DATABASE {{cookiecutter.project_slug}};'
 
 echo
 
-echo 'Creating DB user {{cookiecutter.project_slug}}_user with password {{cookiecutter.database_user_password}}'
-sudo -u postgres psql --command 'CREATE USER {{cookiecutter.project_slug}}_user WITH PASSWORD  '\'{{cookiecutter.database_user_password}}\';
+echo 'Creating DB user {{cookiecutter.database_user}} with password {{cookiecutter.database_user_password}}'
+sudo -u postgres psql --command 'CREATE USER {{cookiecutter.database_user}} WITH PASSWORD  '\'{{cookiecutter.database_user_password}}\';
 
 echo
 
 echo 'Altering role to utf8'
-sudo -u postgres psql --command 'ALTER ROLE {{cookiecutter.project_slug}}_user SET client_encoding TO '\'utf8\';
+sudo -u postgres psql --command 'ALTER ROLE {{cookiecutter.database_user}} SET client_encoding TO '\'utf8\';
 
 echo
 
 echo 'setting default_transaction_isolation TO read committed' 
-sudo -u postgres psql --command 'ALTER ROLE {{cookiecutter.project_slug}}_user SET default_transaction_isolation TO '\'read\ committed\';
+sudo -u postgres psql --command 'ALTER ROLE {{cookiecutter.database_user}} SET default_transaction_isolation TO '\'read\ committed\';
 
 echo
 
 echo 'setting timezone to {{cookiecutter.timezone}}'
-sudo -u postgres psql --command 'ALTER ROLE {{cookiecutter.project_slug}}_user SET timezone TO '\'{{cookiecutter.timezone}}\';
+sudo -u postgres psql --command 'ALTER ROLE {{cookiecutter.database_user}} SET timezone TO '\'{{cookiecutter.timezone}}\';
 
 echo
 
-echo  'Granting all privileges on Database {{cookiecutter.project_slug}} to  {{cookiecutter.project_slug}}_user;'
-sudo -u postgres psql --command 'GRANT ALL PRIVILEGES ON DATABASE {{cookiecutter.project_slug}} TO {{cookiecutter.project_slug}}_user;'
+echo  'Granting all privileges on Database {{cookiecutter.project_slug}} to  {{cookiecutter.database_user}};'
+sudo -u postgres psql --command 'GRANT ALL PRIVILEGES ON DATABASE {{cookiecutter.project_slug}} TO {{cookiecutter.database_user}};'
