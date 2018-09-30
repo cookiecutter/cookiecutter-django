5e554a4cd22c52dfcd3dff7ab15fad3624d8f3d3
myvault <vaultofpython@outlook.com>
2018-09-30 09:49:47 +0300

    Removing ignored files

--- {{cookiecutter.project_slug}}/git_create_repo.sh
+++
@@ -1,5 +0,0 @@
-git init
-git add .
-git commit -m "first {{cookiecutter.project_slug}} commit"
-git remote add origin git@bitbucket.org:myvault/{{cookiecutter.project_slug}}.git
-git push -u origin master
