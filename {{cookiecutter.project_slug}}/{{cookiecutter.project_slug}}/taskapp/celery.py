173574233a84b2d64bfdfc618c6fc89ada2705d2
Guilherme Fabrizio <gfabricio@gmail.com>
2018-10-01 17:17:51 +0100

    fix typo

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/taskapp/celery.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/taskapp/celery.py
@@ -2,8 +2,7 @@
 import os
 from celery import Celery
 
-import sentry_sdk
-from sentry_sdk.integrations.logging import LoggingIntegration
+
 
 from django.apps import apps, AppConfig
 from django.conf import settings
@@ -29,7 +28,7 @@ class CeleryAppConfig(AppConfig):
         app.config_from_object('django.conf:settings', namespace='CELERY')
         installed_apps = [app_config.name for app_config in apps.get_app_configs()]
         app.autodiscover_tasks(lambda: installed_apps, force=True)
-
+{% if cookiecutter.use_sentry == 'y' -%}
 {% if cookiecutter.use_pycharm == 'y' -%}
 	    # Since raven is required in production only,
             # imports might (most surely will) be wiped out
