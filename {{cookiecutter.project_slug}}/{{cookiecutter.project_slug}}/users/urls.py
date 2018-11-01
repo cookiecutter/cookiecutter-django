820ccd2fb6da54f984fe983b38bda15daa22f1ad
Demetris Stavrou <demestav@gmail.com>
2018-11-01 13:40:10 +0200

    Debug check already exists in the main url file. No need to check here again.

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/urls.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/urls.py
@@ -9,12 +9,9 @@ from {{ cookiecutter.project_slug }}.users.views import (
 )
 
 app_name = "users"
-urlpatterns = []
-
-if settings.DEBUG:
-    urlpatterns += [
-        path("", view=user_list_view, name="list"),
-        path("~redirect/", view=user_redirect_view, name="redirect"),
-        path("~update/", view=user_update_view, name="update"),
-        path("<str:username>/", view=user_detail_view, name="detail"),
-    ]
+urlpatterns += [
+    path("", view=user_list_view, name="list"),
+    path("~redirect/", view=user_redirect_view, name="redirect"),
+    path("~update/", view=user_update_view, name="update"),
+    path("<str:username>/", view=user_detail_view, name="detail"),
+]
