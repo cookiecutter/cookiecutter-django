072b8c469ead4694cb8287a332ccb790f230c8ae
Michael A. Gonzalez <genomics.geek.04.22@gmail.com>
2018-12-06 20:09:08 -0500

    Updated react dependencies

--- {{cookiecutter.project_slug}}/frontend/src/index.js
+++ {{cookiecutter.project_slug}}/frontend/src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-import registerServiceWorker from './registerServiceWorker'
+import * as serviceWorker from './serviceWorker';
 
 import Raven from 'raven-js'
 
@@ -21,4 +21,4 @@ if (module.hot && process.env.NODE_ENV === "development") {
   })
 }
 
-registerServiceWorker()
+serviceWorker.unregister()
