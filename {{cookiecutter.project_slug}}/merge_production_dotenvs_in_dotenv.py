f3258f54530aaccf417a79c62fe13b1f4bdf8a59
Demetris Stavrou <demestav@gmail.com>
2019-03-14 23:10:40 +0200

    Documentation update

--- {{cookiecutter.project_slug}}/merge_production_dotenvs_in_dotenv.py
+++ {{cookiecutter.project_slug}}/merge_production_dotenvs_in_dotenv.py
@@ -8,7 +8,7 @@ PRODUCTION_DOTENVS_DIR_PATH = os.path.join(ROOT_DIR_PATH, ".envs", ".production"
 PRODUCTION_DOTENV_FILE_PATHS = [
     os.path.join(PRODUCTION_DOTENVS_DIR_PATH, ".django"),
     os.path.join(PRODUCTION_DOTENVS_DIR_PATH, ".postgres"),
-    os.path.join(PRODUCTION_DOTENVS_DIR_PATH, ".caddy"),
+    os.path.join(PRODUCTION_DOTENVS_DIR_PATH, ".traefik"),
 ]
 DOTENV_FILE_PATH = os.path.join(ROOT_DIR_PATH, ".env")
 
