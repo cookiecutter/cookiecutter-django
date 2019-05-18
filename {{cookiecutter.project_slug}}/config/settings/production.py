d90406c3c3489c896407b09fada43ccfbae544a7
Bruno Alla <alla.brunoo@gmail.com>
2019-05-18 14:06:05 +0100

    Make the static storage use public ACL

--- {{cookiecutter.project_slug}}/config/settings/production.py
+++ {{cookiecutter.project_slug}}/config/settings/production.py
@@ -117,6 +117,7 @@ from storages.backends.s3boto3 import S3Boto3Storage  # noqa E402
 
 class StaticRootS3Boto3Storage(S3Boto3Storage):
     location = "static"
+    default_acl = "public-read"
 
 
 class MediaRootS3Boto3Storage(S3Boto3Storage):
