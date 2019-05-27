761720bce79e8f68db6e0e409d74e906b35d9c0e
Bruno Alla <alla.brunoo@gmail.com>
2019-05-27 16:57:55 +0100

    Prepare 2.1.8-01 tag

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except ImportError:
 
 # Our version ALWAYS matches the version of Django we support
 # If Django has a new release, we branch, tag, then update this setting after the tag.
-version = "2.0.2"
+version = "2.1.8-01"
 
 if sys.argv[-1] == "tag":
     os.system('git tag -a %s -m "version %s"' % (version, version))
