14697391095ba211e53a22c648ad691c4eeebd67
Dani Hodovic <dani.hodovic@gmail.com>
2019-07-09 09:07:38 +0800

    Remove EMAIL_HOST & EMAIL_PORT with locmem backend
    These settings should not be required since Django never connects to an
    external component when sending email. Instead it's stored in memory.
    
    https://docs.djangoproject.com/en/2.2/topics/email/#in-memory-backend

--- {{cookiecutter.project_slug}}/config/settings/test.py
+++ {{cookiecutter.project_slug}}/config/settings/test.py
@@ -48,10 +48,6 @@ TEMPLATES[0]["OPTIONS"]["loaders"] = [  # noqa F405
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
 EMAIL_BACKEND = "django.core.mail.backends.locmem.EmailBackend"
-# https://docs.djangoproject.com/en/dev/ref/settings/#email-host
-EMAIL_HOST = "localhost"
-# https://docs.djangoproject.com/en/dev/ref/settings/#email-port
-EMAIL_PORT = 1025
 
 # Your stuff...
 # ------------------------------------------------------------------------------
