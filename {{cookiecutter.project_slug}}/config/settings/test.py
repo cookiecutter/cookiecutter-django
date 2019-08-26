180ab9bbb192fc0e648c67e52f3d7701792b552f
Dani Hodovic <dani.hodovic@gmail.com>
2019-08-26 16:25:36 +0200

    Remove redundant DEBUG = False in test.py
    This is already set to False by default in base.py

--- {{cookiecutter.project_slug}}/config/settings/test.py
+++ {{cookiecutter.project_slug}}/config/settings/test.py
@@ -7,8 +7,6 @@ from .base import env
 
 # GENERAL
 # ------------------------------------------------------------------------------
-# https://docs.djangoproject.com/en/dev/ref/settings/#debug
-DEBUG = False
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env(
     "DJANGO_SECRET_KEY",
