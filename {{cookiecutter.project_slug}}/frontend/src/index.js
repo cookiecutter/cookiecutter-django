7306a9031782afb90bdf9b80cfb456a95ccfd689
genomics-geek <gonzalezma@email.chop.edu>
2019-10-05 10:41:02 -0400

    [closes #10] - Added React Router

--- {{cookiecutter.project_slug}}/frontend/src/index.js
+++ {{cookiecutter.project_slug}}/frontend/src/index.js
@@ -1,22 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import * as serviceWorker from './serviceWorker'
+
 import './index.css'
-import App from './App'
-import * as serviceWorker from './serviceWorker';
-
-import Raven from 'raven-js'
-
-
-if (process.env.NODE_ENV === 'production') {
-  Raven.config(process.env.REACT_APP_SENTRY_URL).install()
-}
+import Root from './root'
 
 const rootElement = document.getElementById('root')
 
-ReactDOM.render(<App />, rootElement)
-if (module.hot && process.env.NODE_ENV === "development") {
-  module.hot.accept('./App', () => {
-    const NextRoot = require('./App').default
+ReactDOM.render(<Root />, rootElement)
+
+if (module.hot && process.env.NODE_ENV === 'development') {
+  module.hot.accept('./root', () => {
+    const NextRoot = require('./root').default
     ReactDOM.render(<NextRoot />, rootElement)
   })
 }
