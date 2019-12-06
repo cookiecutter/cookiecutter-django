113ce88d9cc9b7af133d114d551af93a509e7774
Ivan Khomutov <i.khomutov@protonmail.com>
2019-12-06 10:55:00 +0300

    Add option to choose CI tool

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -70,7 +70,7 @@ def remove_heroku_files():
     for file_name in file_names:
         if (
             file_name == "requirements.txt"
-            and "{{ cookiecutter.use_travisci }}".lower() == "y"
+            and "{{ cookiecutter.ci_tool }}".lower() == "travis"
         ):
             # don't remove the file if we are using travisci but not using heroku
             continue
@@ -103,6 +103,10 @@ def remove_celery_files():
 
 def remove_dottravisyml_file():
     os.remove(".travis.yml")
+
+
+def remove_dotgitlabciyml_file():
+    os.remove(".gitlab-ci.yml")
 
 
 def append_to_project_gitignore(path):
@@ -349,8 +353,11 @@ def main():
         if "{{ cookiecutter.use_docker }}".lower() == "y":
             remove_celery_compose_dirs()
 
-    if "{{ cookiecutter.use_travisci }}".lower() == "n":
+    if "{{ cookiecutter.ci_tool }}".lower() != "travis":
         remove_dottravisyml_file()
+
+    if "{{ cookiecutter.ci_tool }}".lower() != "gitlab":
+        remove_dotgitlabciyml_file()
 
     print(SUCCESS + "Project initialized, keep up the good work!" + TERMINATOR)
 
