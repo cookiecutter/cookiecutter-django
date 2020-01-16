2f9d69398063025a59e7b2159f345af4bbaeb4ea
howiezhao <howiezhaohr@hotmail.com>
2020-01-16 11:31:37 +0800

    style: formatting code

--- {{cookiecutter.project_slug}}/docs/conf.py
+++ {{cookiecutter.project_slug}}/docs/conf.py
@@ -12,6 +12,7 @@
 #
 import os
 import sys
+
 # import django
 # sys.path.insert(0, os.path.abspath('..'))
 # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
@@ -30,16 +31,15 @@ author = "{{ cookiecutter.author_name }}"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -47,9 +47,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "alabaster"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
