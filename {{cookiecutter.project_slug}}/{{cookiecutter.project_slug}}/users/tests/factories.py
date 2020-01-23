fd43059e289e15296015b7a2eff9db9b8b1ff5e1
Bruno Alla <alla.brunoo@gmail.com>
2020-01-23 17:52:17 +0000

    Update formatting

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/tests/factories.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/tests/factories.py
@@ -12,14 +12,18 @@ class UserFactory(DjangoModelFactory):
 
     @post_generation
     def password(self, create: bool, extracted: Sequence[Any], **kwargs):
-        password = extracted if extracted else Faker(
-            "password",
-            length=42,
-            special_chars=True,
-            digits=True,
-            upper_case=True,
-            lower_case=True,
-        ).generate(extra_kwargs={})
+        password = (
+            extracted
+            if extracted
+            else Faker(
+                "password",
+                length=42,
+                special_chars=True,
+                digits=True,
+                upper_case=True,
+                lower_case=True,
+            ).generate(extra_kwargs={})
+        )
         self.set_password(password)
 
     class Meta:
