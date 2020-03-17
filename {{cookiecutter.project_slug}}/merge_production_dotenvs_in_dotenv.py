984b5bcfbecf61f154b88bcb036fbba1fd717c65
Bruno Alla <alla.brunoo@gmail.com>
2020-03-17 15:46:55 +0000

    Nicer syntax to join paths

--- {{cookiecutter.project_slug}}/merge_production_dotenvs_in_dotenv.py
+++ {{cookiecutter.project_slug}}/merge_production_dotenvs_in_dotenv.py
@@ -5,7 +5,7 @@ from typing import Sequence
 import pytest
 
 ROOT_DIR_PATH = Path(__file__).parent.resolve()
-PRODUCTION_DOTENVS_DIR_PATH = ROOT_DIR_PATH.joinpath(".envs", ".production")
+PRODUCTION_DOTENVS_DIR_PATH = ROOT_DIR_PATH / ".envs" / ".production"
 PRODUCTION_DOTENV_FILE_PATHS = [
     PRODUCTION_DOTENVS_DIR_PATH / ".django",
     PRODUCTION_DOTENVS_DIR_PATH / ".postgres",
