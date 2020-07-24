c57b8976b09fcd6ec7f3ef095b4b967f3d68e8d3
Andrew Chen Wang <60190294+Andrew-Chen-Wang@users.noreply.github.com>
2020-07-24 11:29:11 -0400

    Update {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/utils/context_processors.py
    Co-authored-by: Bruno Alla <browniebroke@users.noreply.github.com>

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/utils/context_processors.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/utils/context_processors.py
@@ -2,5 +2,7 @@ from django.conf import settings
 
 
 def settings_context(_request):
-    # Be explicit
+    """Settings available by default to the templates context."""
+    # Note: we intentionally do NOT expose the entire settings
+    # to prevent accidental leaking of sensitive information
     return {"DEBUG": settings.DEBUG}
