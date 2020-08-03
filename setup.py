4451f4505e0028cd5a5c36c608f67e3500a172b8
Bruno Alla <browniebroke@users.noreply.github.com>
2020-08-03 10:00:47 +0200

    Update template version to 3.0.8-01

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except ImportError:
 
 # Our version ALWAYS matches the version of Django we support
 # If Django has a new release, we branch, tag, then update this setting after the tag.
-version = "3.0.8"
+version = "3.0.8-01"
 
 if sys.argv[-1] == "tag":
     os.system(f'git tag -a {version} -m "version {version}"')
