a2c4453cf56adba84471875d1b08bb0cb77d7002
Bruno Alla <browniebroke@users.noreply.github.com>
2020-09-16 09:39:02 +0100

    Fix isort errors

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/forms.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/forms.py
@@ -1,5 +1,5 @@
-from django.contrib.auth import get_user_model
 from django.contrib.auth import forms as admin_forms
+from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 
