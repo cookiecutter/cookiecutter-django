3869ac8af630a0e39906687137acf5dc8cb27186
Bruno Alla <browniebroke@users.noreply.github.com>
2020-10-04 14:57:51 +0100

    Fix UserFactory for factory-boy 3.1+

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/tests/factories.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/tests/factories.py
@@ -1,7 +1,5 @@
-from typing import Any, Sequence
-
 from django.contrib.auth import get_user_model
-from factory import Faker, post_generation
+from factory import Faker, PostGenerationMethodCall
 from factory.django import DjangoModelFactory
 
 
@@ -10,22 +8,7 @@ class UserFactory(DjangoModelFactory):
     username = Faker("user_name")
     email = Faker("email")
     name = Faker("name")
-
-    @post_generation
-    def password(self, create: bool, extracted: Sequence[Any], **kwargs):
-        password = (
-            extracted
-            if extracted
-            else Faker(
-                "password",
-                length=42,
-                special_chars=True,
-                digits=True,
-                upper_case=True,
-                lower_case=True,
-            ).generate(extra_kwargs={})
-        )
-        self.set_password(password)
+    password = PostGenerationMethodCall("set_password", "password")
 
     class Meta:
         model = get_user_model()
