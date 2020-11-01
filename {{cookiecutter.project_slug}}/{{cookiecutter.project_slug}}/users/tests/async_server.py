ad80fc2fdad9a70c2e76cd5ddde5959e423874eb
Andrew Chen Wang <60190294+Andrew-Chen-Wang@users.noreply.github.com>
2020-11-01 09:36:06 -0500

    Ignore type in port

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/tests/async_server.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/tests/async_server.py
@@ -23,8 +23,8 @@ def run_server(app, path="/"):
     server_state = ServerState()
     protocol = functools.partial(H11Protocol, config=config, server_state=server_state)
     create_server_task = loop.create_server(protocol, host="127.0.0.1")
-    server = loop.run_until_complete(create_server_task)  # type: ignore 
-    port = server.sockets[0].getsockname()[1]
+    server = loop.run_until_complete(create_server_task) 
+    port = server.sockets[0].getsockname()[1]  # type: ignore
     url = "ws://127.0.0.1:{port}{path}".format(port=port, path=path)
     try:
         # Run the event loop in a new thread.
