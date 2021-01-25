d1409e2fb3962c66f8c7829f6b2456b50aca40e5
Bruno Alla <browniebroke@users.noreply.github.com>
2021-01-25 21:08:33 +0000

    Missing import

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/admin.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth import admin as auth_admin
 from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
 
 from {{ cookiecutter.project_slug }}.users.forms import UserChangeForm, UserCreationForm
 
