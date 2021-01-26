125f1a8afe56950f012e40ee9109628526c85f99
Arnav Choudhury <arnav13@gmail.com>
2021-01-26 10:34:18 +0530

    Fixing Formatting issues

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/views.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/views.py
@@ -29,7 +29,7 @@ class UserUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
 
     def get_object(self):
         return self.request.user
-        
+
 
 user_update_view = UserUpdateView.as_view()
 
