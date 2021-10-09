00ff825b8c29099294deecdd7539b0a1276690d3
Dalton Rardin <8892319+dalrrard@users.noreply.github.com>
2021-10-09 01:09:46 -0500

    Add djangorestframework-stubs
    Add djangorestframework-stubs to local.txt requirements
    Modify setup.cfg to add stubs to mypy settings
    Fix new type error in DRF
    
    `./users/api/views.py:19: error: Incompatible type for lookup 'id': (got "Union[AutoField[Union[Combinable, int, str], int], Any]", expected "Union[str, int]")`
    
    by asserting that `self.request.user.id` is an int

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/api/views.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/api/views.py
@@ -16,6 +16,7 @@ class UserViewSet(RetrieveModelMixin, ListModelMixin, UpdateModelMixin, GenericV
     lookup_field = "username"
 
     def get_queryset(self, *args, **kwargs):
+        assert isinstance(self.request.user.id, int)
         return self.queryset.filter(id=self.request.user.id)
 
     @action(detail=False, methods=["GET"])
