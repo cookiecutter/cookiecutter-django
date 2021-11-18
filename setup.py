183ca2088a38f11e811697e733be5037bcba7f7e
Bruno Alla <browniebroke@users.noreply.github.com>
2021-11-18 09:36:42 +0000

    Switch template to calendar versioning & automate releases (#3415)

--- setup.py
+++ setup.py
@@ -1,21 +1,11 @@
 #!/usr/bin/env python
-
-import os
-import sys
-
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
 
-# Our version ALWAYS matches the version of Django we support
-# If Django has a new release, we branch, tag, then update this setting after the tag.
-version = "3.1.13-01"
-
-if sys.argv[-1] == "tag":
-    os.system(f'git tag -a {version} -m "version {version}"')
-    os.system("git push --tags")
-    sys.exit()
+# We use calendar versioning
+version = "2021.11.17"
 
 with open("README.rst") as readme_file:
     long_description = readme_file.read()
