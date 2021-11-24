315ead02e4a24f4bd213141c0a61c79508bf1eca
Bruno Alla <browniebroke@users.noreply.github.com>
2021-11-24 08:05:53 +0000

    Upgrade to Django 3.2 (#3425)
    * Upgrade to Django 3.2
    
    * Set DEFAULT_AUTO_FIELD setting
    
    * Use auto AppConfig discovery for users app

--- {{cookiecutter.project_slug}}/config/settings/base.py
+++ {{cookiecutter.project_slug}}/config/settings/base.py
@@ -48,6 +48,8 @@ DATABASES = {
 }
 {%- endif %}
 DATABASES["default"]["ATOMIC_REQUESTS"] = True
+# https://docs.djangoproject.com/en/stable/ref/settings/#std:setting-DEFAULT_AUTO_FIELD
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # URLS
 # ------------------------------------------------------------------------------
@@ -86,7 +88,7 @@ THIRD_PARTY_APPS = [
 ]
 
 LOCAL_APPS = [
-    "{{ cookiecutter.project_slug }}.users.apps.UsersConfig",
+    "{{ cookiecutter.project_slug }}.users",
     # Your stuff: custom apps go here
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
