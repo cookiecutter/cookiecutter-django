584b50c258c930dc856bc0038d936cf7b244231d
Bruno Alla <alla.brunoo@gmail.com>
2021-12-26 16:17:44 +0000

    Build the HTML for the documentation as part of the CI

--- tests/test_docker.sh
+++ tests/test_docker.sh
@@ -38,3 +38,6 @@ docker-compose -f local.yml run django python manage.py makemessages --all
 
 # Make sure the check doesn't raise any warnings
 docker-compose -f local.yml run django python manage.py check --fail-level WARNING
+
+# Generate the HTML for the documentation
+docker-compose -f local.yml run docs make html
