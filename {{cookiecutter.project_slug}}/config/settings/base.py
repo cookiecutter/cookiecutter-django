785df85c4d14849d7ad0655b028ba50fd8c9186f
Will Gordon <wgordon@redhat.com>
2021-12-27 06:37:33 -0500

    Hides 'sign up' elements when ACCOUNT_ALLOW_REGISTRATION is disabled (#1914)
    Co-authored-by: Bruno Alla <alla.brunoo@gmail.com>

--- {{cookiecutter.project_slug}}/config/settings/base.py
+++ {{cookiecutter.project_slug}}/config/settings/base.py
@@ -197,6 +197,7 @@ TEMPLATES = [
                 "django.template.context_processors.static",
                 "django.template.context_processors.tz",
                 "django.contrib.messages.context_processors.messages",
+                "{{cookiecutter.project_slug}}.users.context_processors.allauth_settings",
             ],
         },
     }
