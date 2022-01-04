1010b05d52d15dff791e90e524bb6bff9a6627d4
Bruno Alla <alla.brunoo@gmail.com>
2022-01-04 13:17:21 +0000

    Fix sequence name

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/contrib/sites/migrations/0003_set_site_domain_and_name.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/contrib/sites/migrations/0003_set_site_domain_and_name.py
@@ -25,11 +25,11 @@ def _update_or_create_site_with_sequence(site_model, connection, domain, name):
         # greater than the maximum value.
         max_id = site_model.objects.order_by('-id').first().id
         with connection.cursor() as cursor:
-            cursor.execute("SELECT last_value from django_site_id")
+            cursor.execute("SELECT last_value from django_site_id_seq")
             (current_id,) = cursor.fetchone()
             if current_id < max_id:
                 cursor.execute(
-                    "alter sequence django_site_id restart with %s",
+                    "alter sequence django_site_id_seq restart with %s",
                     [max_id + 1],
                 )
                 cursor.fetchone()
