b5b20c80d3b753b27c84a19c9da1c2753a1e5906
Bruno Alla <browniebroke@users.noreply.github.com>
2022-01-07 17:59:16 +0000

    Add missing blank line

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/forms.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/forms.py
@@ -17,6 +17,7 @@ class UserCreationForm(admin_forms.UserCreationForm):
     Form for User Creation in the Admin Area.
     To change user signup, see UserSignupForm and UserSocialSignupForm.
     """
+
     class Meta(admin_forms.UserCreationForm.Meta):
         model = User
 
