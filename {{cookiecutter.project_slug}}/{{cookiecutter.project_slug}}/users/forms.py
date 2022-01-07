2021b5ceca9a76c24b54ead9f5b4c9d57bc00515
mpoli <poli@closedgap.com>
2022-01-07 11:08:54 -0300

    Sorting imports for isort
    Co-authored-by: Bruno Alla <browniebroke@users.noreply.github.com>

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/forms.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/forms.py
@@ -1,7 +1,7 @@
-from django.contrib.auth import forms as admin_forms
-from django.contrib.auth import get_user_model
 from allauth.account.forms import SignupForm
 from allauth.socialaccount.forms import SignupForm as SocialSignupForm
+from django.contrib.auth import forms as admin_forms
+from django.contrib.auth import get_user_model
 from django.utils.translation import gettext_lazy as _
 
 User = get_user_model()
