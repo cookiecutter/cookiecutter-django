ca8470adaf9a9b6cbdd49cfafad624222c2ea1a7
mpoli <poli@closedgap.com>
2022-01-07 14:02:42 -0300

    Remove save override in UserSignupForm
    Co-authored-by: Bruno Alla <browniebroke@users.noreply.github.com>

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/forms.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/forms.py
@@ -31,18 +31,6 @@ class UserSignupForm(SignupForm):
     Check UserSocialSignupForm for accounts created from social.
     """
 
-    def save(self, request):
-
-        # Ensure you call the parent class's save.
-        # .save() returns a User object.
-        user = super(UserSignupForm, self).save(request)
-
-        # Add your own processing here.
-
-        # You must return the original result.
-        return user
-
-
     class Meta:
         model = User
     
