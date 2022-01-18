6c45bf2d49dfd6d52a00a840c2f1af27644635f5
Vikas Yadav <vikas.yadav@iiitb.org>
2022-01-18 22:09:44 +0530

    Apply suggestions from code review
    Co-authored-by: Fábio C. Barrionuevo da Luz <bnafta@gmail.com>
    Co-authored-by: Bruno Alla <browniebroke@users.noreply.github.com>

--- {{cookiecutter.project_slug}}/config/settings/base.py
+++ {{cookiecutter.project_slug}}/config/settings/base.py
@@ -336,11 +336,26 @@ REST_FRAMEWORK = {
 CORS_URLS_REGEX = r"^/api/.*$"
 
 # By Default swagger ui is available only to admin user. You can change permission classs to change that
+# See more configuration options at https://drf-spectacular.readthedocs.io/en/latest/settings.html#settings
 SPECTACULAR_SETTINGS = {
-    "TITLE": "API documentation for{{ cookiecutter.project_slug }}",
-    "DESCRIPTION": "Lets rock and then roll!",
+    "TITLE": "{{ cookiecutter.project_name }} API",
+    "DESCRIPTION": "Documentation of API endpoiints of {{ cookiecutter.project_name }}",
     "VERSION": "1.0.0",
     "SERVE_PERMISSIONS": ["rest_framework.permissions.IsAdminUser"],
+    "SERVERS": [
+        {
+            "url": "https://127.0.0.1:8000",
+            "description": "Local Development server"
+        },
+#        {
+#            "url": "https://staging.{{ cookiecutter.domain_name }}",
+#            "description": "Development server"
+#        },
+        {
+            "url": 'https://{{ cookiecutter.domain_name }}",
+            "description": "Production server"
+        }
+    ],
 }
 {%- endif %}
 # Your stuff...
