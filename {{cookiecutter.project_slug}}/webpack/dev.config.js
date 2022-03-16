4e690bf5fca9a8b8e5de2cd2b873b16a65ca2ef4
Bruno Alla <alla.brunoo@gmail.com>
2022-03-16 22:21:16 +0000

    Fix a couple of issues with Webpack + Docker

--- {{cookiecutter.project_slug}}/webpack/dev.config.js
+++ {{cookiecutter.project_slug}}/webpack/dev.config.js
@@ -7,7 +7,11 @@ module.exports = merge(commonConfig, {
   devServer: {
     port: 3000,
     proxy: {
+      {%- if cookiecutter.use_docker == 'n' %}
       '/': 'http://0.0.0.0:8000',
+      {%- else %}
+      '/': 'http://django:8000',
+      {%- endif %}
     },
     // We need hot=false (Disable HMR) to set liveReload=true
     hot: false,
