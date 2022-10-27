a307231de27b90d1d9f4140a20c84f08bf84a143
Alejandro Franco <jafrancov@gmail.com>
2022-10-27 10:24:13 -0500

    User migrations

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/migrations/0001_initial.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/migrations/0001_initial.py
@@ -1,6 +1,3 @@
-import {{ cookiecutter.project_slug }}.users.models
-import django.contrib.auth.models
-import django.contrib.auth.validators
 from django.db import migrations, models
 import django.utils.timezone
 import uuid
