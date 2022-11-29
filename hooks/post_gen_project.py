5c37807754b1ef04c59ef536a7c0d678397d3a7c
alexzvk <alexzvk@umich.edu>
2022-11-28 23:13:40 -0500

    fiddling with licenses.json file pathing

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -331,7 +331,7 @@ def handle_licenses():
     with open(os.path.join("licenses", "-temporary-placeholder.txt")) as f:
         selected_title = f.readline()
 
-    with open(os.path.join("..", "licenses.json")) as f:
+    with open(os.path.join(os.pardir, "licenses.json")) as f:
         titles_dict = json.load(f)
     # access the title to filename dictionary to find the correct file
     # using a dictionary instead of looping reduces time complexity
