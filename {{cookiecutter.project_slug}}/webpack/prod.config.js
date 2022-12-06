fca83113205c5c0c9a5fe643531211eadf2c2955
Bruno Alla <alla.brunoo@gmail.com>
2022-12-06 11:59:34 +0000

    Add support for Azure Storage + Webpack

--- {{cookiecutter.project_slug}}/webpack/prod.config.js
+++ {{cookiecutter.project_slug}}/webpack/prod.config.js
@@ -2,14 +2,18 @@ const { merge } = require('webpack-merge');
 const commonConfig = require('./common.config');
 
 // This variable should mirror the one from config/settings/production.py
-{%- if cookiecutter.use_whitenoise == 'n' and cookiecutter.cloud_provider == 'AWS' %}
+{%- if cookiecutter.use_whitenoise == 'n' %}
+{%- if cookiecutter.cloud_provider == 'AWS' %}
 const s3BucketName = process.env.DJANGO_AWS_STORAGE_BUCKET_NAME;
 const awsS3Domain = process.env.DJANGO_AWS_S3_CUSTOM_DOMAIN ?
     process.env.DJANGO_AWS_S3_CUSTOM_DOMAIN
     : `${s3BucketName}.s3.amazonaws.com`;
 const staticUrl = `https://${awsS3Domain}/static/`;
-{%- elif cookiecutter.use_whitenoise == 'n' and cookiecutter.cloud_provider == 'GCP' %}
+{%- elif cookiecutter.cloud_provider == 'GCP' %}
 const staticUrl = `https://storage.googleapis.com/${process.env.DJANGO_GCP_STORAGE_BUCKET_NAME}/static/`;
+{%- elif cookiecutter.cloud_provider == 'Azure' %}
+const staticUrl = `https://${process.env.DJANGO_AZURE_ACCOUNT_NAME}.blob.core.windows.net/static/`;
+{%- endif %}
 {%- else %}
 const staticUrl = '/static/';
 {%- endif %}
