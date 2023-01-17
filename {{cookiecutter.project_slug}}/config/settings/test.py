35cfb8cc88af21b0f93423a0fba545c4da2fa570
Jeh <jbanimineni@packershift.io>
2023-01-17 11:57:53 +0530

    Fix typo on the test settings

--- {{cookiecutter.project_slug}}/config/settings/test.py
+++ {{cookiecutter.project_slug}}/config/settings/test.py
@@ -25,7 +25,7 @@ PASSWORD_HASHERS = ["django.contrib.auth.hashers.MD5PasswordHasher"]
 # https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
 EMAIL_BACKEND = "django.core.mail.backends.locmem.EmailBackend"
 
-# DEBUGING FOR TEMPLATES
+# DEBUGGING FOR TEMPLATES
 # ------------------------------------------------------------------------------
 TEMPLATES[0]["OPTIONS"]["debug"] = True  # type: ignore # noqa F405
 
