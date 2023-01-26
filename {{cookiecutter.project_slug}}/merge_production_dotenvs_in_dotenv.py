43699e333ba35c7fdf61cd49c5f27494c85388bb
example <example@example.com>
2023-01-26 09:20:19 +0100

    Rename BASE_DIR_PATH to BASE_DIR

--- {{cookiecutter.project_slug}}/merge_production_dotenvs_in_dotenv.py
+++ {{cookiecutter.project_slug}}/merge_production_dotenvs_in_dotenv.py
@@ -4,13 +4,13 @@ from pathlib import Path
 
 import pytest
 
-BASE_DIR_PATH = Path(__file__).parent.resolve()
-PRODUCTION_DOTENVS_DIR_PATH = BASE_DIR_PATH / ".envs" / ".production"
+BASE_DIR = Path(__file__).parent.resolve()
+PRODUCTION_DOTENVS_DIR_PATH = BASE_DIR / ".envs" / ".production"
 PRODUCTION_DOTENV_FILE_PATHS = [
     PRODUCTION_DOTENVS_DIR_PATH / ".django",
     PRODUCTION_DOTENVS_DIR_PATH / ".postgres",
 ]
-DOTENV_FILE_PATH = BASE_DIR_PATH / ".env"
+DOTENV_FILE_PATH = BASE_DIR / ".env"
 
 
 def merge(
