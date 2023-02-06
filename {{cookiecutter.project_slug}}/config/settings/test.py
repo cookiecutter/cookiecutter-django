d4423d5166f44e7f6fe551af8898fbbc2742f160
Bruno Alla <alla.brunoo@gmail.com>
2023-02-06 14:49:03 +0000

    Replace DummyWebpackLoader by the one from webpack_loader package

--- {{cookiecutter.project_slug}}/config/settings/test.py
+++ {{cookiecutter.project_slug}}/config/settings/test.py
@@ -34,7 +34,7 @@ TEMPLATES[0]["OPTIONS"]["debug"] = True  # type: ignore # noqa F405
 # ------------------------------------------------------------------------------
 WEBPACK_LOADER["DEFAULT"][  # noqa F405
     "LOADER_CLASS"
-] = "{{cookiecutter.project_slug}}.utils.loaders.DummyWebpackLoader"
+] = "webpack_loader.loader.FakeWebpackLoader"
 
 {%- endif %}
 # Your stuff...
