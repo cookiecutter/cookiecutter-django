c3fa66c11155c4d7b0db1f4019b53014649c1b75
M-Usman-Dev <usman.wahab@arbisoft.com>
2023-03-28 20:03:59 +0500

    social google login added

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/urls.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/urls.py
@@ -12,6 +12,6 @@ urlpatterns = [
     path("~redirect/", view=user_redirect_view, name="redirect"),
     path("~update/", view=user_update_view, name="update"),
     path("<str:username>/", view=user_detail_view, name="detail"),
-        path('rest-auth/google/', GoogleLogin.as_view(), name='google_login'),
+    path('rest-auth/google/', GoogleLogin.as_view(), name='google_login'),
 
 ]
