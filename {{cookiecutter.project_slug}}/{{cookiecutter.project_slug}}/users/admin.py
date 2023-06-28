a4d71a8df6e8b943195bd5f40e060208c3c0e497
Bruno Alla <alla.brunoo@gmail.com>
2023-06-28 23:21:40 +0100

    Fix mypy and add a comment to allauth documentation

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/admin.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/admin.py
@@ -9,8 +9,9 @@ from {{ cookiecutter.project_slug }}.users.forms import UserAdminChangeForm, Use
 User = get_user_model()
 
 if settings.DJANGO_ADMIN_FORCE_ALLAUTH:
-    # Force the `admin` sign in process to go through the `django-allauth` workflow
-    admin.site.login = decorators.login_required(admin.site.login)
+    # Force the `admin` sign in process to go through the `django-allauth` workflow:
+    # https://django-allauth.readthedocs.io/en/stable/advanced.html#admin
+    admin.site.login = decorators.login_required(admin.site.login)  # type: ignore[method-assign]
 
 
 @admin.register(User)
