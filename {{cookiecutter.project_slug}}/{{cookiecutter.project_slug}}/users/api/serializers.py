fe2800ecd329798cd64056f178209051c7d27ee4
Bruno Alla <alla.brunoo@gmail.com>
2023-06-29 12:18:28 +0200

    Improve type hints for UserSerializer

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/api/serializers.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/api/serializers.py
@@ -1,10 +1,18 @@
+from __future__ import annotations
+
+import typing
+
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
+
+if typing.TYPE_CHECKING:
+    from {{ cookiecutter.project_slug }}.users.models import User as UserType
+
 
 User = get_user_model()
 
 
-class UserSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer[UserType]):
     class Meta:
         model = User
         {%- if cookiecutter.username_type == "email" %}
