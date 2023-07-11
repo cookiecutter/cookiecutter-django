18fd1ea85b9d6558bf95e8b46ff72dd94b0f8b8f
Bruno Alla <alla.brunoo@gmail.com>
2023-07-11 13:28:17 +0100

    Add French translations

--- {{cookiecutter.project_slug}}/config/settings/base.py
+++ {{cookiecutter.project_slug}}/config/settings/base.py
@@ -30,7 +30,8 @@ LANGUAGE_CODE = "en-us"
 # from django.utils.translation import gettext_lazy as _
 # LANGUAGES = [
 #     ('en', _('English')),
-#     ('pt-br', _('Português')),
+#     ('fr-fr', _('French')),
+#     ('pt-br', _('Portuguese')),
 # ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#site-id
 SITE_ID = 1
