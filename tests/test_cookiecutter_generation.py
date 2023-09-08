a7dfdd3f436430f295c578a0f6ba54557f8cba9c
Alejandro Franco <jafrancov@gmail.com>
2023-09-08 00:08:36 -0600

    Upgrade CC-Dj 2023-09-07

--- tests/test_cookiecutter_generation.py
+++ tests/test_cookiecutter_generation.py
@@ -43,6 +43,7 @@ def context():
 
 
 SUPPORTED_COMBINATIONS = [
+    {"username_type": "email"},
     {"open_source_license": "MIT"},
     {"open_source_license": "BSD"},
     {"open_source_license": "GPLv3"},
