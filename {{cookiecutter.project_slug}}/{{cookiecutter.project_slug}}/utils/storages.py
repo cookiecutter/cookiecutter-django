85fada8b5a073e68b12909999a1bdbeea20aa2b7
Bruno Alla <alla.brunoo@gmail.com>
2023-09-12 10:30:49 +0100

    Remove "Root" in our own storage class names

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/utils/storages.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/utils/storages.py
@@ -2,35 +2,35 @@
 from storages.backends.s3 import S3Storage
 
 
-class StaticRootS3Storage(S3Storage):
+class StaticS3Storage(S3Storage):
     location = "static"
     default_acl = "public-read"
 
 
-class MediaRootS3Storage(S3Storage):
+class MediaS3Storage(S3Storage):
     location = "media"
     file_overwrite = False
 {%- elif cookiecutter.cloud_provider == 'GCP' -%}
 from storages.backends.gcloud import GoogleCloudStorage
 
 
-class StaticRootGoogleCloudStorage(GoogleCloudStorage):
+class StaticGoogleCloudStorage(GoogleCloudStorage):
     location = "static"
     default_acl = "publicRead"
 
 
-class MediaRootGoogleCloudStorage(GoogleCloudStorage):
+class MediaGoogleCloudStorage(GoogleCloudStorage):
     location = "media"
     file_overwrite = False
 {%- elif cookiecutter.cloud_provider == 'Azure' -%}
 from storages.backends.azure_storage import AzureStorage
 
 
-class StaticRootAzureStorage(AzureStorage):
+class StaticAzureStorage(AzureStorage):
     location = "static"
 
 
-class MediaRootAzureStorage(AzureStorage):
+class MediaAzureStorage(AzureStorage):
     location = "media"
     file_overwrite = False
 {%- endif %}
