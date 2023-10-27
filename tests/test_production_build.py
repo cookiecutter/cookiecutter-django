1e2d0b3825550d73ee8b4c21192be2db3bdb1e5c
Ahmed <u7490701@anu.edu.au>
2023-10-27 17:36:12 +1100

    Changes made to the files

--- tests/test_production_build.py
+++ tests/test_production_build.py
@@ -1,5 +1,7 @@
+import os
 import subprocess
 
 def test_production_check():
-    # This will raise an exception if there's an error
-    subprocess.check_call(['python', 'manage.py', 'check', '--deploy'])
+    base_dir = os.getcwd()
+    manage_py_path = os.path.join(base_dir, "test_project", "manage.py")
+    subprocess.check_call(['python', manage_py_path, 'check', '--deploy'])
