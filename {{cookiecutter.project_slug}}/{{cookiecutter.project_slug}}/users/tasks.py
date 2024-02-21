b756d904d77e992242caeba4fbd188bccfa0c782
Matthew Foster Walsh <15671892+mfosterw@users.noreply.github.com>
2024-02-21 05:01:56 -0500

    You probably don't need `get_user_model` (#4879)
    * You probably don't need `get_user_model`
    Fixes #4872
    
    * Fix Flake8 (add space after import)
    
    * (try to) fix ruff
    
    * Fix import format

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/tasks.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/tasks.py
@@ -1,8 +1,5 @@
-from django.contrib.auth import get_user_model
-
 from config import celery_app
-
-User = get_user_model()
+from {{ cookiecutter.project_slug }}.users.models import User
 
 
 @celery_app.task()
