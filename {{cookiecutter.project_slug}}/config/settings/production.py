61f11cefc2a2ec42c7bc289e861c1c63c7a36226
Alex Kanavos <alexkanv@gmail.com>
2024-08-05 12:57:43 +0200

    change email subject prefix variable name in settings

--- {{cookiecutter.project_slug}}/config/settings/production.py
+++ {{cookiecutter.project_slug}}/config/settings/production.py
@@ -224,8 +224,8 @@ DEFAULT_FROM_EMAIL = env(
 # https://docs.djangoproject.com/en/dev/ref/settings/#server-email
 SERVER_EMAIL = env("DJANGO_SERVER_EMAIL", default=DEFAULT_FROM_EMAIL)
 # https://docs.djangoproject.com/en/dev/ref/settings/#email-subject-prefix
-EMAIL_SUBJECT_PREFIX = env(
-    "DJANGO_EMAIL_SUBJECT_PREFIX",
+ACCOUNT_EMAIL_SUBJECT_PREFIX = env(
+    "DJANGO_ACCOUNT_EMAIL_SUBJECT_PREFIX",
     default="[{{cookiecutter.project_name}}] ",
 )
 
