830a7c5467802c9a135401292fb45bf569cd9c85
Bruno Alla <alla.brunoo@gmail.com>
2024-10-17 19:38:27 +0100

    Fix incorrect option to overwrite_files for AzureStorage
    https://django-storages.readthedocs.io/en/latest/backends/azure.html

--- {{cookiecutter.project_slug}}/config/settings/production.py
+++ {{cookiecutter.project_slug}}/config/settings/production.py
@@ -170,7 +170,7 @@ STORAGES = {
         "BACKEND": "storages.backends.azure_storage.AzureStorage",
         "OPTIONS": {
             "location": "media",
-            "file_overwrite": False,
+            "overwrite_files": False,
         },
     },
     {%- if cookiecutter.use_whitenoise == 'y' %}
