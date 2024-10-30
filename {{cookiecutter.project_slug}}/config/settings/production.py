9bef46c198d42523f4ec212a989d34281ab7ca93
Christian Jensen <christian@jensenbox.com>
2024-10-30 00:13:20 -0700

    Small spelling correction (#5502)
    Mimicing > Mimicking

--- {{cookiecutter.project_slug}}/config/settings/production.py
+++ {{cookiecutter.project_slug}}/config/settings/production.py
@@ -41,7 +41,7 @@ CACHES = {
         "LOCATION": REDIS_URL,
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
-            # Mimicing memcache behavior.
+            # Mimicking memcache behavior.
             # https://github.com/jazzband/django-redis#memcached-exceptions-behavior
             "IGNORE_EXCEPTIONS": True,
         },
