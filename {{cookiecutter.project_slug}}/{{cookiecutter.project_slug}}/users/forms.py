6db4acbec59c70da5be949e778dd00b1a958e88b
fravetier <fravetier@vikta.com>
2025-03-24 16:08:52 +0100

    Added # type: ignore[name-defined] on UserAdminCreationForm

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/forms.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/forms.py
@@ -17,7 +17,7 @@ class UserAdminChangeForm(admin_forms.UserChangeForm):
         {%- endif %}
 
 
-class UserAdminCreationForm(admin_forms.AdminUserCreationForm):
+class UserAdminCreationForm(admin_forms.AdminUserCreationForm):  # type: ignore[name-defined]
     """
     Form for User Creation in the Admin Area.
     To change user signup, see UserSignupForm and UserSocialSignupForm.
