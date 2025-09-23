69e1d9603e4aceaf1c0e84ae0c4ed3dfdcf64bdd
Matthew Foster Walsh <15671892+mfosterw@users.noreply.github.com>
2025-09-23 10:46:11 -0600

    Added help command to sphinx Makefile (#6024)
    Co-authored-by: Bruno Alla <alla.brunoo@gmail.com>

--- {{cookiecutter.project_slug}}/docs/make.bat
+++ {{cookiecutter.project_slug}}/docs/make.bat
@@ -12,7 +12,7 @@ set SOURCEDIR=_source
 set BUILDDIR=_build
 set APP=..\{{cookiecutter.project_slug}}
 
-if "%1" == "" goto html
+if "%1" == "" goto help
 
 %SPHINXBUILD% >NUL 2>NUL
 if errorlevel 9009 (
@@ -39,8 +39,8 @@ GOTO :EOF
 sphinx-apidoc -o %SOURCEDIR%/api %APP%
 GOTO :EOF
 
-:html
-%SPHINXBUILD% -b html %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
+:help
+%SPHINXBUILD% -M help %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
 
 :end
 popd
