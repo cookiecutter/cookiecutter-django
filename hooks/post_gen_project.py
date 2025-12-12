bea8f870503524fa8cbaa8581f6c53ffbf3b5b5d
Kyriakos Toumbas <kyriakos@toumbas.com.cy>
2025-12-12 22:52:16 +0200

    Move FE config project files to proper location

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -148,7 +148,6 @@ def handle_js_runner(choice, use_docker, use_async):
                 "mini-css-extract-plugin",
                 "postcss-loader",
                 "postcss-preset-env",
-                "sass-loader",
                 "webpack",
                 "webpack-bundle-tracker",
                 "webpack-cli",
@@ -176,7 +175,6 @@ def handle_js_runner(choice, use_docker, use_async):
             "gulp-plumber",
             "gulp-postcss",
             "gulp-rename",
-            "gulp-sass",
             "gulp-uglify-es",
         ]
         if not use_docker:
