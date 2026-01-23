98d4d67624878ae59500cd0dc04d0b11bdbb8049
Pulkit Agrawal <plktagrawal@gmail.com>
2026-01-23 11:59:53 +0530

    fix: Make Ninja Schema Align with Django Model
    The Schema defined fields which were not part of the Django user model. This was causing the Django container to shutdown after an error.

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/api/schema.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/api/schema.py
@@ -2,9 +2,12 @@ from ninja import Schema, ModelSchema
 from {{ cookiecutter.project_slug }}.users.models import User
 
 class UserSchema(ModelSchema):
+    url: str
+
     class Meta:
         model = User
-        fields = ["username", "name", "url"]
-        extra = "readonly"
+        exclude = ["password"]
 
-    url: str
+    @staticmethod
+    def resolve_url(obj):
+        return obj.get_absolute_url()
